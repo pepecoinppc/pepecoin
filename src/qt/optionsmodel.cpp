4a5eae8ae5b3d516fd261a1768484e9d48d33bfa
David Eichel <david22782@gmail.com>
2023-10-25 16:24:28 -0400

    Update tx urls in gui to point to new pepecoin blockchain explorer

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -76,7 +76,7 @@ void OptionsModel::Init(bool resetSettings)
     nDisplayUnit = settings.value("nDisplayUnit").toInt();
 
     if (!settings.contains("strThirdPartyTxUrls"))
-        settings.setValue("strThirdPartyTxUrls", "");
+        settings.setValue("strThirdPartyTxUrls", "https://explorer.getpepecoin.com/tx/%s");
     strThirdPartyTxUrls = settings.value("strThirdPartyTxUrls", "").toString();
 
     if (!settings.contains("fCoinControlFeatures"))
