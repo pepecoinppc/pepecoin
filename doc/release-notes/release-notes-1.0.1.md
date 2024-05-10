7cc114115076c92731af77e7d18e4191636223bf
David Eichel <david22782@gmail.com>
2024-05-10 01:35:22 -0400

    Update version number to 1.0.1. Update nMinimumChainWork and defaultAssumeValid in chainparams.cpp

---
+++ doc/release-notes/release-notes-1.0.1.md
@@ -0,0 +1,7 @@
+##Pepecoin Core 1.0.1 Release Notes
+
+####Technical
+
+####Design
+
+####Documentation
--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -110,10 +110,10 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 0; // Disabled
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0000000000000000000000000000000000000000000000000000000000010001"); // genesis block
+        consensus.nMinimumChainWork = uint256S("0000000000000000000000000000000000000000000000014c0089faf46fdf69"); // genesis block
 
         // By default assume that the signatures in ancestors of this block are valid.
-        consensus.defaultAssumeValid = uint256S("0x37981c0c48b8d48965376c8a42ece9a0838daadb93ff975cb091f57f8c2a5faa"); // genesis block
+        consensus.defaultAssumeValid = uint256S("0xfcc4aec940dc1a11b287dfcd1eeceb4537c5aca3565ba2bd9928a9d44f70f25a"); // genesis block
 
         // AuxPoW parameters
         consensus.nAuxpowChainId = 0x003f; // 63
